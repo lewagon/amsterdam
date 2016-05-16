80f0a0776e899bf32e3b6bf22aca75c90ee841bd
Cecile Veneziani <contact@cecilitse.org>
2016-05-16 16:17:28 +0200

    Enhance students helper
    - Remove id attribute on student
    - Remove last attribute on last student

--- helpers/students_helper.rb
+++ helpers/students_helper.rb
@@ -3,10 +3,11 @@ module StudentsHelper
     attributes    = []
     students      = data.students["batch_#{batch}"]
     student_index = students.index(student)
+    student_id    = student_index + 1
     prev_student  = students[student_index - 1]
     next_student  = students[student_index + 1]
 
-    attributes << "id=\"#{student.id}\""
+    attributes << "id=\"#{student_id}\""
     attributes << "data-href=\"#{student.url}\""
 
     if student.fake
@@ -14,14 +15,14 @@ module StudentsHelper
     end
 
     if prev_student
-      prev_student_id = prev_student.id
+      prev_student_id = student_id - 1
       prev_student_id -= 1 if prev_student.fake
 
       attributes << "data-prev=\"#{prev_student_id}\""
     end
 
-    if next_student && !next_student.last
-      next_student_id = next_student.id
+    if next_student != students.last
+      next_student_id = student_id + 1
       next_student_id += 1 if next_student.fake
 
       attributes << "data-next=\"#{next_student_id}\""
